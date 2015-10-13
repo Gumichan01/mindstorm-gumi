9c660c312da387b718e1d2324292c03bd5613fec
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-10-13 16:45:58 +0200

    Moteur fait

--- hello_ev3/src/EV3gumi/Hello.java
+++ hello_ev3/src/EV3gumi/Hello.java
@@ -5,22 +5,32 @@ import lejos.hardware.BrickFinder;
 import lejos.hardware.Sound;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
 
 
 public class Hello{
 	
+	private static RegulatedMotor motor;
+
 	public static void main(String [] args){
 		
 		// Recupère l'adresse du cerveau
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
-		TextLCD lcd = ev3.getTextLCD();		// Objet représentant l'écran LCD
+		
+		motor = new EV3LargeRegulatedMotor(MotorPort.A);
+	
+		TextLCD lcd = ev3.getTextLCD();		
 		
 		// On met le texte sur l'écran LCD
 		lcd.drawString("Hello World!", 4, 4);
 		Sound.beep();
-		// Attendre 2 secondes
+		Delay.msDelay(1000);
+		
+		motor.rotate(3600);
 		Delay.msDelay(2000);
 
 	}
