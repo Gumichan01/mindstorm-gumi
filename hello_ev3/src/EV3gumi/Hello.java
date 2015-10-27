b7a0e59d41f06ecfed97d7a0be8bd505847e893b
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-10-27 13:56:13 +0100

    Mise à jour du programme hello

--- hello_ev3/src/EV3gumi/Hello.java
+++ hello_ev3/src/EV3gumi/Hello.java
@@ -1,12 +1,17 @@
 package EV3gumi;
 
 
+
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Sound;
 import lejos.hardware.ev3.EV3;
+import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
@@ -22,17 +27,21 @@ public class Hello{
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
 		
 		motor = new EV3LargeRegulatedMotor(MotorPort.A);
-	
-		TextLCD lcd = ev3.getTextLCD();		
+		
+		TextLCD lcd = ev3.getTextLCD();
 		
 		// On met le texte sur l'écran LCD
 		lcd.drawString("Hello World!", 4, 4);
-		Sound.beep();
+		//Sound.beep();
 		Delay.msDelay(1000);
 		
-		motor.rotate(3600);
+		// Rotation
+		motor.rotate(720);
 		Delay.msDelay(2000);
 
+		
+		
+		
 	}
 	
 }
