eb8d50b1d97960cce5569b2bc29c1a2ed9311299
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-11-11 20:25:30 +0100

    Le robot peut avancer, faire demi-tour, revenir à son point de départ et se repositionner.(C'est assez approximatif)

--- hello_ev3/src/EV3gumi/Hello.java
+++ hello_ev3/src/EV3gumi/Hello.java
@@ -3,24 +3,20 @@ package EV3gumi;
 
 
 import lejos.hardware.BrickFinder;
-import lejos.hardware.Sound;
 import lejos.hardware.ev3.EV3;
-import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.Port;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
 
-
+// Avancer, demi--tour, avancer, demi-tour (c'est un peu approximatif)
 public class Hello{
 
 	private static RegulatedMotor motor,motor2;
-	private static int angle = 3600; 
+	private static int angle = 1800;
+	private static int angleRotation = 465;
 	
 	public static void main(String [] args){
 
@@ -48,21 +44,46 @@ public class Hello{
 			}
 		}).start();
 		
-		motor2.rotate(angle);
+		motor2.rotate(angle+16);
 
-
-		
 		Delay.msDelay(100);
 		
+		// Demi-tour
 		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
-				motor.rotate(-angle);
+				motor.rotate(-angleRotation);
 			}
 		}).start();
 		
-		motor2.rotate(-angle);
+		motor2.rotate(angleRotation);
+		
+		Delay.msDelay(100);
+		
+		// Avancer
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				motor.rotate(angle);
+			}
+		}).start();
+		
+		motor2.rotate(angle);
+
+		Delay.msDelay(100);
+		
+		// Demi-tour (repositionnement)
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				motor.rotate(angleRotation);
+			}
+		}).start();
+		
+		motor2.rotate(-angleRotation);
 		
 		Delay.msDelay(2000);
 
