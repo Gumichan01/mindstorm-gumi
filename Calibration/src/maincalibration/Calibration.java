7fc80aad8852a78b48175b12fd00c7b3f5af43a7
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-11-13 17:37:55 +0100

    Moyenne des couleurs

--- Calibration/src/maincalibration/Calibration.java
+++ Calibration/src/maincalibration/Calibration.java
@@ -9,29 +9,44 @@ import mstorm_sensor.SensorType;
 
 public class Calibration {
 
+	public final static int NUMCHANS = 3;
 	
 	public static void main(String [] args){
 		
-		//int avg
+		float r_avg, g_avg, b_avg;
 		float [][] sample;
-		
 		
 		LightAndColorSensor sensor = new LightAndColorSensor(LocalEV3.get().
 				getPort(LightAndColorSensor.sensor_port));
 		
 		int sz = sensor.getRGBMode().sampleSize();
-		sample = new float[3][sz];
+		sample = new float[NUMCHANS][sz];
 		
-		for(int i = 0; i <3; i++){
+		// 3 color Detections
+		for(int i = 0; i <NUMCHANS; i++){
 			
 			sample[i] = sensor.fetch(SensorType.COLOR_SENSOR);
-			System.out.println("Color : " + Arrays.toString(sample[i]));
+			//System.out.println("Color : " + Arrays.toString(sample[i]));
 			Delay.msDelay(1000);
 		}
 		
-		for(int j = 0; j < 3; j++)
-			
+		// Average
+		r_avg = 0;
+		g_avg = 0;
+		b_avg = 0;
 		
+		for(int j = 0; j < NUMCHANS; j++){
+			r_avg += sample[j][0];
+			g_avg += sample[j][1];
+			b_avg += sample[j][2];
+		}
+			
+		r_avg /= NUMCHANS;
+		g_avg /= NUMCHANS;
+		b_avg /= NUMCHANS;
+		
+		System.out.println("AVG : " + r_avg + "\n" + g_avg + "\n" + b_avg);
+		Delay.msDelay(8000);
 		
 		sensor.close();
 	}
