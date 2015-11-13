a23e7050fdccc54c2aeb3ea6fefc283123868fc9
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-11-13 18:31:36 +0100

    Mise à jour du module de detection

--- projet-long/src/mstorm_sensor/LightAndColorSensor.java
+++ projet-long/src/mstorm_sensor/LightAndColorSensor.java
@@ -1,11 +1,10 @@
 package mstorm_sensor;
 
-import java.util.Arrays;
 
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
+
 
 
 public class LightAndColorSensor extends EV3ColorSensor{
@@ -13,7 +12,6 @@ public class LightAndColorSensor extends EV3ColorSensor{
 	public final static String sensor_port = "S1";
 	private final static float light_value = 0.42f;
 	private final static float dark_value = 0.02f;
-	private final static int delay =2000;
 	
 	private SampleProvider color_provider;
 	private SampleProvider light_provider;
@@ -25,26 +23,15 @@ public class LightAndColorSensor extends EV3ColorSensor{
 		color_provider = this.getRGBMode();
 		light_provider = this.getAmbientMode();
 		
-		calibrate();
+		setScale();
 	}
 
 	/**
-	 * 
-	 * 	Sensor calibration
-	 * 
-	 * */
-	private void calibrate(){
+	 * 	Set the minimal scale
+	 */
+	private void setScale(){
 		
-		//float [] light = new float[light_provider.sampleSize()];
-		
-		System.out.println("Calibration");
-	
-		//light_provider.fetchSample(light, 0);
-		Delay.msDelay(delay);
-		//light_provider.fetchSample(light, 0);
-	
 		scale = 1 / (light_value - dark_value);
-		//System.out.println("Ambient light: "+Arrays.toString(light));
 		System.out.println("Scale: "+scale);
 	}
 	
@@ -60,12 +47,8 @@ public class LightAndColorSensor extends EV3ColorSensor{
 			values = fetchLightSample();
 	    }
 		
-		System.out.println("Values: "+Arrays.toString(values));
-		
-		Delay.msDelay(2000);
-		
+		//System.out.println("Values: "+Arrays.toString(values));
 		for(int i = 0; i < values.length;i++){
-			
 			values[i] *= scale; 
 		}
 		
@@ -77,7 +60,6 @@ public class LightAndColorSensor extends EV3ColorSensor{
 	private float [] fetchColorSample(){
 		
 		float [] sample = new float[color_provider.sampleSize()];
-		
 		color_provider.fetchSample(sample, 0);
 		
 		return sample;
@@ -86,7 +68,6 @@ public class LightAndColorSensor extends EV3ColorSensor{
 	private float [] fetchLightSample(){
 		
 		float [] sample = new float[light_provider.sampleSize()];
-		
 		light_provider.fetchSample(sample, 0);
 		
 		return sample;
