7578d6948a355b7e4c068df96d2eb254aec1ced4
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-12 17:59:17 +0200

    Angle : Fixed the units.

--- src/dev/trace/AngleCalculation.java
+++ src/dev/trace/AngleCalculation.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class AngleCalculation {
 
-	private static final float DISTANCE_2PI = 17.165f;	// cm
+	private static final float DISTANCE_2PI = 0.17165f;	// meter
 	private static final int PI_2 = 360;
-	private static final float L = 10f;					// cm
+	private static final float L = 0.10f;				// meter
 	
 	ArrayList<Float> theta_list;
 	
@@ -22,14 +22,13 @@ public class AngleCalculation {
 			
 			float [] darray = new float[]{rl.get(i).getVL()*DISTANCE_2PI/PI_2,
 											rl.get(i).getVR()*DISTANCE_2PI/PI_2};
-
-			
 			float [] varray = new float[]{darray[0],darray[1]};
 			
 			if(i < rl.size() - 1){
 				
 				// seconds
 				float difft = (rl.get(i+1).getTime() - rl.get(i).getTime()) / 1000f;
+				/* Is that correct ? */
 				darray[0] *= difft;
 				darray[1] *= difft;
 			}
