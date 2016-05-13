66ec496a0a2dfc65a75e247543052adc72bb6bea
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-13 09:51:12 +0200

    data

--- src/dev/trace/AngleTest.java
+++ src/dev/trace/AngleTest.java
@@ -9,14 +9,13 @@ public class AngleTest {
 		AngleCalculation c = new AngleCalculation();
 		ArrayList<RobotDatum> rl = new ArrayList<>();
 		
-		rl.add(new RobotDatum(360, 0, 0));
-		rl.add(new RobotDatum(0, 0, 1000));
-		//rl.add(new RobotDatum(360, 360, 30));
-		//rl.add(new RobotDatum(360, 240, 2035));
-		//rl.add(new RobotDatum(180, 360, 3537));
-		//rl.add(new RobotDatum(360, 360, 6535));
-		//rl.add(new RobotDatum(-360, 360, 8539));
-		//rl.add(new RobotDatum(0, 0, 10449));
+		rl.add(new RobotDatum(0, 0, 0));
+		rl.add(new RobotDatum(360, 360, 30));
+		rl.add(new RobotDatum(360, 240, 2035));
+		rl.add(new RobotDatum(180, 360, 3537));
+		rl.add(new RobotDatum(360, 360, 6535));
+		rl.add(new RobotDatum(-360, 360, 8539));
+		rl.add(new RobotDatum(0, 0, 10449));
 	
 		c.generateData(rl);
 		System.out.println(c.toString());
