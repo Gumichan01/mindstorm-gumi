b3dfdab76e92ed17e685935b79c5f8000f830445
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-27 14:09:47 +0200

    Fixed a bug in the color checker

--- src/main/mstorm_colorcheck/ColorChecker.java
+++ src/main/mstorm_colorcheck/ColorChecker.java
@@ -11,7 +11,9 @@ public class ColorChecker {
 	private static final float MARGIN = 2.0f;
 	private static float [] line_sample;	// Line color
 	private float [] bg_sample;				// Background color
-	private float [] ht_sample;				// Half-turn color
+	private float [] black_sample;			// Black color
+	private float [] blue_sample;			// Blue color
+	private float [] ch_sample;				// Choice color
 	private float [] st_sample;				// Stop color
 	private float epsilon;					// ε for the error margin
 	
@@ -20,30 +22,33 @@ public class ColorChecker {
 		try{
 			line_sample = new float[NB_COLORS];
 			bg_sample = new float[NB_COLORS];
-			ht_sample = new float[NB_COLORS];
+			black_sample = new float[NB_COLORS];
+			blue_sample = new float[NB_COLORS];
 			st_sample = new float[NB_COLORS];
+			ch_sample = new float[NB_COLORS];
 
 			BufferedReader reader = new BufferedReader(new FileReader("line.gumi")); 
 			BufferedReader rd = new BufferedReader(new FileReader("bg.gumi"));
 			BufferedReader rdht = new BufferedReader(new FileReader("ht.gumi"));
+			BufferedReader rdht2 = new BufferedReader(new FileReader("ht2.gumi"));
 			BufferedReader rdst = new BufferedReader(new FileReader("stop.gumi"));
+			BufferedReader rdch = new BufferedReader(new FileReader("choice.gumi"));
 			
 			for(int i = 0 ; i < NB_COLORS; i++){
-				
-				String s = reader.readLine();
-				String ss = rd.readLine();
-				String sss = rdht.readLine();
-				String ssss = rdst.readLine();
-				line_sample[i] = Float.parseFloat(s);
-				bg_sample[i] = Float.parseFloat(ss);
-				ht_sample[i] = Float.parseFloat(sss);
-				st_sample[i] = Float.parseFloat(ssss);
+
+				line_sample[i] = Float.parseFloat(reader.readLine());
+				bg_sample[i] = Float.parseFloat(rd.readLine());
+				black_sample[i] = Float.parseFloat(rdht.readLine());
+				blue_sample[i] = Float.parseFloat(rdht2.readLine());
+				st_sample[i] = Float.parseFloat(rdst.readLine());
+				ch_sample[i] = Float.parseFloat(rdch.readLine());
 			}
 			
 			reader.close();
 			rd.close();
 			rdht.close();
 			rdst.close();
+			rdch.close();
 			
 			epsilon = Math.abs(euclide_distance(line_sample, bg_sample))/MARGIN;
 			
@@ -60,13 +65,12 @@ public class ColorChecker {
 		if(sample_to_check.length != NB_COLORS)
 			return false;
 
-		float dist_bg, dist_line, dist_ht, dist_st;
+		float dist_bg, dist_line, dist_st;
 		dist_line = euclide_distance(sample_to_check, line_sample);
 		dist_bg = euclide_distance(sample_to_check, bg_sample);
-		dist_ht = euclide_distance(sample_to_check, ht_sample);
 		dist_st = euclide_distance(sample_to_check, st_sample);
 		
-		return (dist_line <= dist_bg) && (dist_line <= dist_ht) && (dist_line <= dist_st);
+		return (dist_line <= dist_bg) && (dist_line <= dist_st);
 	}
 
 	/// Check if the color is the background color
@@ -75,28 +79,27 @@ public class ColorChecker {
 		if(sample_to_check.length != NB_COLORS)
 			return false;
 
-		float dist_bg, dist_line, dist_ht, dist_st;
+		float dist_bg, dist_line, dist_st;
 		dist_line = euclide_distance(sample_to_check, line_sample);
 		dist_bg = euclide_distance(sample_to_check, bg_sample);
-		dist_ht = euclide_distance(sample_to_check, ht_sample);
 		dist_st = euclide_distance(sample_to_check, st_sample);
 		
-		return (dist_line >= dist_bg) &&  (dist_ht >= dist_bg) && (dist_st >= dist_bg);
+		return (dist_line >= dist_bg) && (dist_st >= dist_bg);
 	}
 
-	/// Check if the color is the half-turn color
-	public boolean isHalfTurncColor(float [] sample_to_check) throws Exception{
+	/// Check if the color is the choice color
+	public boolean isChoiceColor(float [] sample_to_check) throws Exception{
 		
 		if(sample_to_check.length != NB_COLORS)
 			return false;
 
-		float dist_bg, dist_line, dist_ht, dist_st;
+		float dist_bg, dist_line, dist_color, dist_st;
 		dist_line = euclide_distance(sample_to_check, line_sample);
 		dist_bg = euclide_distance(sample_to_check, bg_sample);
-		dist_ht = euclide_distance(sample_to_check, ht_sample);
 		dist_st = euclide_distance(sample_to_check, st_sample);
+		dist_color = euclide_distance(sample_to_check, ch_sample);
 		
-		return (dist_ht <= dist_line) && (dist_ht <= dist_bg) && (dist_ht <= dist_st);
+		return (dist_color <= dist_line) && (dist_color <= dist_bg) && (dist_color <= dist_st);
 	}
 	
 	/// Check if the color is the stop color
@@ -105,13 +108,12 @@ public class ColorChecker {
 		if(sample_to_check.length != NB_COLORS)
 			return false;
 
-		float dist_bg, dist_line, dist_ht, dist_st;
+		float dist_bg, dist_line, dist_st;
 		dist_line = euclide_distance(sample_to_check, line_sample);
 		dist_bg = euclide_distance(sample_to_check, bg_sample);
-		dist_ht = euclide_distance(sample_to_check, ht_sample);
 		dist_st = euclide_distance(sample_to_check, st_sample);
 		
-		return (dist_st <= dist_line) && (dist_st <= dist_bg) && (dist_st <= dist_ht);
+		return (dist_st <= dist_line) && (dist_st <= dist_bg);
 	}
 	
 	/// Check if this is the border
@@ -127,6 +129,17 @@ public class ColorChecker {
 		return Math.abs(dist_line - dist_bg) <= epsilon;
 	}
 	
+	public boolean isBlackColor(float [] sample_to_check) throws Exception{
+		
+		if(sample_to_check.length != NB_COLORS)
+			return false;
+
+		float dist_black, dist_bg;;
+		dist_black = euclide_distance(sample_to_check, black_sample);
+		dist_bg = euclide_distance(sample_to_check, bg_sample);
+		
+		return (dist_black <= dist_bg);
+	}
 	
 	private static float euclide_distance(float [] color1,float [] color2){
 		
