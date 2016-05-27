cec5130cb61419c527b1a4f55e68f98590076726
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-27 13:36:31 +0200

    Half turn done

--- src/calibration/half_turn/HTCalibre.java
+++ src/calibration/half_turn/HTCalibre.java
@@ -9,7 +9,10 @@ public class HTCalibre {
 	
 	public static void main(String [] args)throws Exception{
 
+		System.out.println("Started");
+		System.out.println("Black color");
 		CalibrateColorPath.Calibrate(true);
+		System.out.println("Blue color");
 		CalibrateColorPath.Calibrate(false);
 
 	}
